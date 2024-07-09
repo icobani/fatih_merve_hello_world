127938d6679ed78370770b12e2a31993f2ec1c5d
Ibrahim ÇOBANİ <ibrahim@imaconsult.com>
2024-07-09 15:39:01 +0300

    main dosyası eklendi

---
+++ main.go
@@ -0,0 +1,17 @@
+/*
+	B1 Yönetim Sistemleri Yazılım ve Danışmanlık Ltd. Şti.
+	User        : ICI
+	Name        : Ibrahim ÇOBANİ
+	Date        : 09.07.2024 Tuesday
+	Time        : 15:38
+	Notes       :
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
